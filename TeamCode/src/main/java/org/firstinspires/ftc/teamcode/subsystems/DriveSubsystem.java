4a896b0f1ca0b0886f4be2c054220edcdeece95a
PariP970 <parip970@stemia.ca>
2025-12-27 15:08:09 -0700

    updated IMU

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -45,7 +45,7 @@ public class DriveSubsystem extends SubsystemBase {
         ));
 
     }
-
+//m
     public void MecanumDrive(double driveMotion, double turnMotion, double strafeMotion){
         double speedReducer = 0.5;
         double controlledCap = Math.max(Math.abs(driveMotion)+ Math.abs(turnMotion)+ Math.abs(strafeMotion), 1);
